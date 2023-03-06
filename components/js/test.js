c82ed4ac6365d292b16029a081382dd7e45997f4
Jayson Nguyen <buleberry.nguyen@gmail.com>
2023-03-06 10:15:37 +0700

    done request database

---
+++ components/js/test.js
@@ -0,0 +1,47 @@
+const sql = require('mssql');
+
+const config = {
+    user: 'jaysonguyendb', // better stored in an app setting such as process.env.DB_USER
+    password: 'Thanhnguyen1', // better stored in an app setting such as process.env.DB_PASSWORD
+    server: 'jaysonguyendb.database.windows.net', // better stored in an app setting such as process.env.DB_SERVER
+    port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+    database: 'jaysonguyendb', // better stored in an app setting such as process.env.DB_NAME
+    authentication: {
+        type: 'default'
+    },
+    options: {
+        encrypt: true
+    }
+}
+
+console.log("Starting...");
+connectAndQuery();
+
+
+async function connectAndQuery() {
+    try {
+        var poolConnection = await sql.connect(config);
+
+        console.log("Reading rows from the Table...");
+        var resultSet = await poolConnection.request().query(`SELECT * FROM Datmonannhanh`);
+
+        console.log(resultSet.recordset[0].mamonan);
+
+        // output column headers
+        var columns = "";
+        for (var column in resultSet.recordset.columns) {
+            columns += column + ", ";
+        }
+        console.log("%s\t", columns.substring(0, columns.length - 2));
+
+        // ouput row contents from default record set
+        resultSet.recordset.forEach(row => {
+            console.log("%s\t%s", row.CategoryName, row.ProductName);
+        });
+
+        // close connection only when we're certain application is finished
+        poolConnection.close();
+    } catch (err) {
+        console.error(err.message);
+    }
+}
